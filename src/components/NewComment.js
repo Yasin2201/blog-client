843ed94c8a80cf564270eeca1164e80209e9a419
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-09-10 19:34:41 +0100

    get user input from formData and POST to database on form submit then refresh page to re-render comments

--- src/components/NewComment.js
+++ src/components/NewComment.js
@@ -1,15 +1,55 @@
-const NewComment = () => {
+import { useState } from "react";
+
+const NewComment = ({ id }) => {
+    const [errors, setErrors] = useState([]);
+    const API_URL = process.env.REACT_APP_API_URL;
 
     const onNewCommentSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target)
+        const userInput = {
+            username: formData.get('username'),
+            text: formData.get('text'),
+        }
 
+        try {
+            const response = await fetch(`${API_URL}/posts/${id}/comments`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userInput)
+            })
+            const data = await response.json()
+
+            if (data.errors) {
+                setErrors(data.errors)
+            } else {
+                setErrors([])
+                window.location.reload(false);
+            }
+        } catch (err) {
+            console.error(err)
+        }
     }
 
     return (
-        <form className="new-comment-form" onSubmit={(e) => onNewCommentSubmit(e)}>
-            <input type="text" name="username" placeholder="Username" />
-            <input type="text" name="text" placeholder="Share your thoughts..." />
-            <button className="submit-btn" type="submit">Submit</button>
-        </form>
+        <div>
+            <form className="new-comment-form" onSubmit={(e) => onNewCommentSubmit(e)}>
+                <input type="text" name="username" placeholder="Username" />
+                <textarea type="text" name="text" placeholder="Share your thoughts..." />
+                <button className="submit-btn" type="submit">Submit</button>
+            </form>
+            {errors.map((error) => {
+                return (
+                    <div key={errors.indexOf(error)} className="error-message">
+                        {error.msg}!
+                    </div>
+                )
+            })}
+        </div>
+
     )
 }
 
